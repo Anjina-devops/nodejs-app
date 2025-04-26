dfac7d34584f0d02dcbcee5035f1f581b61f2ff3
Anjina-devops <anjina0322@gmail.com>
2025-04-27 02:43:36 +0530

    adding terraform code

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,19 +7,23 @@ variable "region" {
 variable "vpc_cidr" {
   description = "VPC CIDR block"
   type        = string
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   description = "List of CIDRs for public subnets"
   type        = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "List of CIDRs for private subnets"
   type        = list(string)
+  default = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
 }
 
 variable "azs" {
   description = "List of availability zones"
   type        = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
